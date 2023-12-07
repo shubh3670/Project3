00a606dc816410664dda7038f21a3a99ae8d5975
Shubh Patel <shubhpatel6001@gmail.com>
2023-12-07 11:57:38 +0530

    Description of the changes

--- src/components/AnimatedRoutes.jsx
+++ src/components/AnimatedRoutes.jsx
@@ -6,20 +6,21 @@ import Home from '../Pages/Home';          // Adjust the path accordingly
 import Skills from '../Pages/Skills';      // Adjust the path accordingly
 import Brief from '../Pages/Brief';        // Adjust the path accordingly
 import Project from '../Pages/Project';    // Adjust the path accordingly
-import {AnimatePresence} from 'framer-motion'
+import { AnimatePresence } from 'framer-motion';
 
 function AnimatedRoutes() {
   const location = useLocation();
 
   return (
     <AnimatePresence>
-    <Routes location={location} key={location.pathname}>
-      <Route path="/Home" element={<Home />} />
-      <Route path="/" element={<Home />} />
-      <Route path="/Project" element={<Project />} />
-      <Route path="/Skills" element={<Skills />} />
-      <Route path="/Brief" element={<Brief />} />
-    </Routes>
+      <Routes location={location} key={location.pathname}>
+        {/* Set the home page as the default page using the "index" property */}
+        <Route path="/Home" element={<Home />} />
+        <Route path="/" element={<Home />} index />
+        <Route path="/Project" element={<Project />} />
+        <Route path="/Skills" element={<Skills />} />
+        <Route path="/Brief" element={<Brief />} />
+      </Routes>
     </AnimatePresence>
   );
 }
